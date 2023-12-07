1d4561c8eff16e31baa84514e9e9a3a52a2dd9f0
Muhammad Atef <96551959+EngMuhammadAtef@users.noreply.github.com>
2023-12-07 16:35:08 +0200

    optimized crud file

--- crud_operations/crud.py
+++ crud_operations/crud.py
@@ -1,23 +1,41 @@
+RECO_COLLECTION = 'recommendations'
+USERS_COLLECTION = 'users'
+
 def Update_Recom_List(db, nationalId:str, IDs_list: list, Scores_list: list):
     """
-        insert recommendation list of user
+        insert recommendation list for user
 
         Parameters
-        nationalId -> nationalId of user that you want to update his content
-        IDs_list: list -> list of recommended users' IDs list
-        Scores_list: list -> list of recommended users' Scores list
+            nationalId -> nationalId of user that you want to update his content
+            IDs_list: list -> list of recommended users' IDs list
+            Scores_list: list -> list of recommended users' Scores list
     """
-    collection = db['recommendations']
-    response = collection.update_one( {'nationalId': nationalId}, {'$set': {'userRecommendations':[{'nationalId': IDs_list[i], 'score':Scores_list[i]} for i in range(len(IDs_list))]}})
+    try:
+        # get recommendations collection
+        rec_cols = db[RECO_COLLECTION]
+        
+        # Create a list of recommendations
+        recommendations = [{'nationalId': ID, 'score': score} for ID, score in zip(IDs_list, Scores_list)]
+
+        # update recommendations for user nationalId
+        response = rec_cols.update_one( {'nationalId': nationalId}, {'$set': {'userRecommendations':recommendations}})
+    except Exception as e:
+        print(f"An error occurred in Update_Recom_List: {e}")
 
 def get_all_available_IDs(db):
-    # Connect to users collection
-    collection = db['users']
-    users = collection.find({"partnerId":None})
-    NIDs = []
-    for user in users:
-        NIDs.append(user['nationalId'])
-    return NIDs
+    try:
+        # get users collection
+        users_cols = db[USERS_COLLECTION]
+        
+        # get all users have not partners
+        Available_users = users_cols.find({"isAvailable":True})
+
+        # get nationalId of these users
+        return [user['nationalId'] for user in Available_users]
+    
+    except Exception as e:
+        print(f"An error occurred in get_all_available_IDs: {e}")
+        return []
 
 def get_all_content_for_available_users(db):
     """
@@ -26,12 +44,19 @@ def get_all_content_for_available_users(db):
         Return
         IDs, contents -> id and content for all users
     """
-    # find all of users
-    collection = db['recommendations']
-    users = collection.find({"nationalId": {"$in": get_all_available_IDs(db)}})
-    IDs, contents = [], []
+    try:
+        # Find all users with available IDs
+        available_ids = get_all_available_IDs(db)
+        rec_cols = db[RECO_COLLECTION]
 
-    for user in users:
-        IDs.append(user['nationalId'])
-        contents.append(' '.join([ user['fieldOfStudy'], user['specialization'], ''.join((content['skillName']+' ')*content['skillRate'] for content in user['userSkills']) ]))
-    return IDs, contents
+        # get all nationalIds and contents of available users
+        users = rec_cols.find({"nationalId": {"$in": available_ids}})
+        IDs, contents = [], []
+        for user in users:
+            IDs.append(user['nationalId'])
+            contents.append(f"{user['fieldOfStudy']} {user['specialization']} {''.join((content['skillName'] + ' ') * content['skillRate'] for content in user['userSkills'])}")
+        return IDs, contents
+    
+    except Exception as e:
+        print(f"An error occurred in get_all_content_for_available_users: {e}")
+        return [], []
