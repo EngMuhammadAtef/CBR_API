3e81d726432bdd27c0c0f4547f3833daeb65e4c8
Muhammad Atef <96551959+EngMuhammadAtef@users.noreply.github.com>
2024-05-02 01:42:38 +0300

    Update connection.py

--- config/connection.py
+++ config/connection.py
@@ -8,6 +8,6 @@ def connect_to_sql():
         return postgres connection
     """
     # Create a new client and connect to the server from cloud
-    sql_url = "postgres://postgress:viTLZbbS2eREkQ6V5qKwKirxNT90zdha@dpg-con9lda1hbls73ff7gf0-a.oregon-postgres.render.com/study_partner_or6z" # External connection
+    sql_url = "postgres://postgress:viTLZbbS2eREkQ6V5qKwKirxNT90zdha@dpg-con9lda1hbls73ff7gf0-a/study_partner_or6z" # Internal connection
     sql_connection = connect(sql_url)
     return sql_connection
